b400f0b79ddb50b0c923e2d864f4a6c7b8086864
George Campbell <campbell.goe@gmail.com>
2020-04-22 19:26:56 +0100

    implement prettier linter/formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    "plugin:prettier/recommended",
+    "prettier",
     'standard'
   ],
   globals: {
@@ -21,19 +23,24 @@ module.exports = {
   },
   plugins: [
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
+    "prettier"
   ],
   rules: {
     "semi": "error",
+    "max-len": ["error", { "code": 120 }],
+    "comma-dangle": "off",
     "space-before-function-paren":"off",
     "no-unused-expressions":"off",
-    "react/no-unknown-property":"off"
+    "react/no-unknown-property":"off",
+    "react/react-in-jsx-scope": "off", // React is available globally due to Next.js
+    "prettier/prettier": "error"
   },
   overrides: [
     {
       "files": ["enums/*.tsx", "enums/*.ts"],
       "rules": {
-        "no-unused-vars": ["off"]
+        "no-unused-vars": ["off"],
       }
     }
   ]
--- classes/aframe/camera/tsx/camera-rig.tsx
+++ classes/aframe/camera/tsx/camera-rig.tsx
@@ -1,17 +1,18 @@
-import { Entity } from 'aframe-react';
-import Camera from './camera';
+import { Entity } from 'aframe-react'
+import Camera from './camera'
 import FuseCursor from './fuse-cursor'
 
 type Props = {
-  fuseCursor?: boolean,
+  fuseCursor?: boolean
 }
-//TODO: give unique id's to every Entity that exists in a scene - including this one
 
 const CameraRig = ({ fuseCursor }: Props) => {
-  return (<Entity className="camera-rig player-camera">
-      <Camera/>
-      {fuseCursor === true && <FuseCursor/>}
-  </Entity>)
+  return (
+    <Entity className="camera-rig">
+      <Camera />
+      {fuseCursor === true && <FuseCursor />}
+    </Entity>
+  )
 }
 
-export default CameraRig;
+export default CameraRig
